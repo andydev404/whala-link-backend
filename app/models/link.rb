9be7fefb8914b25d6ce908cf059529547c2ffe62
Andy Santana <andy.santana@corotos.com.do>
2022-01-14 08:39:52 -0400

    fix:parameterize the slug

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,11 +2,11 @@ class Link < ApplicationRecord
   validates :long_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
   validates :slug, uniqueness: true
 
-  before_validation :generate_slug
+  before_validation :generate_or_parameterize_slug
 
   private
 
-  def generate_slug
-    self.slug = SecureRandom.uuid[0..6] if slug.nil? || slug.blank?
+  def generate_or_parameterize_slug
+    self.slug = slug.nil? || slug.blank? ? SecureRandom.uuid[0..6] : slug.parameterize
   end
 end
